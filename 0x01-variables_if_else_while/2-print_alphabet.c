a9aaf90fb637c378fdc1a5fccb60ceaaceb04f60
Bisrat Girma <bisratx@gmail.com>
2021-06-18 21:55:36 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main displays all lowercase characters using putchar method 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char lower;
+char newline;
+newline = '\n';
+
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+	putchar(lower);
+}
+putchar(newline);
+return (0);
+}
