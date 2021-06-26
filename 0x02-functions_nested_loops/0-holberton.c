0f1a6119a13e9028d653e8be6dbb9718719c00e7
bisrax <bisratx@gmail.com>
2021-06-26 16:17:54 +0300

    correcting output

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,15 +7,6 @@
 
 int main(void)
 {
-	_putchar("H");
-	_putchar("o");
-	_putchar("l");
-	_putchar("b");
-	_putchar("e");
-	_putchar("r");
-	_putchar("t");
-	_putchar("o");
-	_putchar("n");
-	_putchar("\n");
+	_putchar("Holberton\n");
 	return (0);
 }
