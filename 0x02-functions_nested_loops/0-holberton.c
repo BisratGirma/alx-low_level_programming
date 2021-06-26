4ee95284140484a8ebda69be9d0d42aa72d1ace9
bisrax <bisratx@gmail.com>
2021-06-26 16:29:58 +0300

    the difference

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,5 +8,14 @@
 int main(void)
 {
 	_putchar('Holberton\n');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
