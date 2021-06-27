ef94c94315f9891a73d2f19c09f1cac4447e4bcb
bisrax <bisratx@gmail.com>
2021-06-27 11:19:30 +0300

    first attempt

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - displays all lowercases followed by newline
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int i = 97;
+	while( i < 123 )
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
