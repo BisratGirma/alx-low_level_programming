2df1890c25c1e8edc9ef25835d60b0e1fc70de03
bisrax <bisratx@gmail.com>
2021-06-28 23:07:41 +0300

    correct put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _puts - displays ever character in string.
+ * @str: a character to be printed
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(*(str + i);
+
+	_putchar('\n');
+}
