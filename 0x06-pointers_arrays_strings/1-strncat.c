78dd716432643238191582c6e1f3b54f0d05e031
bisrax <bisratx@gmail.com>
2021-06-30 18:59:15 +0300

    logic implementation

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,20 @@
 #include "holberton.h"
+/**
+ * _strncat - merges two integers with n bytes from src
+ * @dest: first string
+ * @str: second string
+ *
+ * Return: a merged string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int destlen = 0, i;
+
+	while (dest[destlen] != '\0')
+		destlen++;
+
+	for (i = 0; i < n; i++)
+		dest[destlen + i] = src[i];
+
+	return (dest);
+}
