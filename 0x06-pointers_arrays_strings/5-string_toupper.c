9b31d984264f2f75c755f8863f4c1dd96994aad3
bisrax <bisratx@gmail.com>
2021-07-01 18:49:50 +0300

    betty issue fixed

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,6 +3,8 @@
 /**
  * string_toupper - changes every small character to upper
  * @s: a string variable
+ *
+ * Return: an uppercase string.
  */
 char *string_toupper(char *s)
 {
@@ -10,7 +12,7 @@ char *string_toupper(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-	 	if (s[i] >= 'a' && s[i] <= 'z')
+		if (s[i] >= 'a' && s[i] <= 'z')
 			s[i] = s[i] - 32;
 	}
 	return (s);
