ee281d268d136cedf033db1acd7ce26d71c2f0d5
bisrax <bisratx@gmail.com>
2021-07-01 09:33:37 +0300

    strn applied

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copies the content of a string to other.
+ * @src: the first integer to be copied
+ * @dest: the second integer that copies the value of @dest
+ *
+ * Return: a copied value 
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return dest;
+}
