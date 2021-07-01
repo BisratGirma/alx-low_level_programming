e7ee499e2068a63965bb524569f382ac4f5afeb4
bisrax <bisratx@gmail.com>
2021-07-01 23:39:18 +0300

    betty issue

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,27 +8,27 @@
 char *leet(char *str)
 {
 	int i;
-	char mappings[26 * 2];
+	char alpha[26 * 2];
 
 	for (i = 0; i < 26 * 2; i++)
 	{
-		mappings[i] = 0;
+		alpha[i] = 0;
 
-		mappings['a' - 'a'] = mappings['A' - 'A' + 26] = '4';
-		mappings['e' - 'a'] = mappings['E' - 'A' + 26] = '3';
-		mappings['o' - 'a'] = mappings['O' - 'A' + 26] = '0';
-		mappings['t' - 'a'] = mappings['T' - 'A' + 26] = '7';
-		mappings['l' - 'a'] = mappings['L' - 'A' + 26] = '1';
+		alpha['a' - 'a'] = alpha['A' - 'A' + 26] = '4';
+		alpha['e' - 'a'] = alpha['E' - 'A' + 26] = '3';
+		alpha['o' - 'a'] = alpha['O' - 'A' + 26] = '0';
+		alpha['t' - 'a'] = alpha['T' - 'A' + 26] = '7';
+		alpha['l' - 'a'] = alpha['L' - 'A' + 26] = '1';
 	}
 
 	for (i = 0; *(str + i) != '\0'; i++)
 	{
-		int is_lower = *(str + i) >= 'a' && *(str + i) <= 'z';
-		int is_upper = *(str + i) >= 'A' && *(str + i) <= 'Z';
-		int key = *(str + i) - (is_lower * 'a') - (is_upper * ('A' - 26));
+		int l = *(str + i) >= 'a' && *(str + i) <= 'z';
+		int u = *(str + i) >= 'A' && *(str + i) <= 'Z';
+		int key = *(str + i) - (l * 'a') - (u * ('A' - 26));
 
-		if ((is_lower || is_upper) && mappings[key] != 0)
-			*(str + i) = mappings[key];
+		if ((l || u) && alpha[key] != 0)
+			*(str + i) = alpha[key];
 	}
 	return (str);
 }
