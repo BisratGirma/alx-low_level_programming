7349d20105c7b8a393b710770bd827257acd15db
bisrax <bisratx@gmail.com>
2021-07-01 20:18:08 +0300

    hot fix - betty, switch

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,30 +9,30 @@
 char *cap_string(char *s)
 {
 	int i;
-	
+
 	if (s[0] >= 97 && s[0] <= 122)
 		s[0] -= 32;
 
 	for (i = 1; s[i] != '\0'; i++)
 	{
 		switch (s[i])
-		{	
-			case ' '; 
-			case '\t';
-		        case '\n'; 
-			case ','; 
-			case ';'; 
-			case '.'; 
-			case '!'; 
-			case '?'; 
-			case '"'; 
-			case '('; 
-			case ')'; 
-			case '{'; 
-			case '}'; 
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
 				if (s[i + 1] >= 97 && s[i + 1] <= 122)
 					s[i + 1] -= 32;
 		}
+	}
 	return (s);
 }
- 
