4657d044b89e0be13edef1ae762d2c80843b4f0c
bisrax <bisratx@gmail.com>
2021-07-01 20:10:16 +0300

    trying switch

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes a given string.
+ * @s: a string variable
+ *
+ * Return: a capitalized string.
+ */
+char *cap_string(char *s)
+{
+	int i;
+	
+	if (s[0] >= 97 && s[0] <= 122)
+		s[0] -= 32;
+
+	for (i = 1; s[i] != '\0'; i++)
+	{
+		switch (s[i])
+		{	
+			case ' '; 
+			case '\t';
+		        case '\n'; 
+			case ','; 
+			case ';'; 
+			case '.'; 
+			case '!'; 
+			case '?'; 
+			case '"'; 
+			case '('; 
+			case ')'; 
+			case '{'; 
+			case '}'; 
+				if (s[i + 1] >= 97 && s[i + 1] <= 122)
+					s[i + 1] -= 32;
+		}
+	return (s);
+}
+ 
