7a4da27d1deadeb893c7fe13c536d45f9ecdbfe5
Bisrat Girma <bisratx@gmail.com>
2021-07-05 18:39:55 +0300

    clear code 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: The string to be searched.
+ * @accept: The prefix to be measured.
+ *
+ * Return: The number of bytes in s which
+ * consist only of bytes from accept.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	unsigned int j = 0;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				j++;
+				break;
+			}
+
+			else if (accept[i + 1] == '\0')
+				return (j);
+		}
+
+		s++;
+	}
+	return (j);
+}
