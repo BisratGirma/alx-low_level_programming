9f75ace3b03c64be0af91fee62954e2d14afc931
bisrax <bisratx@gmail.com>
2021-07-07 19:36:33 +0300

    04 attempt clear code

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s - a string to be printed.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+# Recursion is Hard i know
+## All code Related to Recursion Project
+** DM for any **
