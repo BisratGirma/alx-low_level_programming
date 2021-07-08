a7593c9b63346cee3c9a33a7d06094e1263db031
bisrax <bisratx@gmail.com>
2021-07-08 22:13:39 +0300

    screaming

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,8 +2,8 @@
 
 /**
  * wildcmp - check if two strings are identical
- * @S1 - first string
- * @s2 - second string
+ * @s1: first string
+ * @s2: second string
  *
  * Return - 1 if identical, and 0 if not.
  */
@@ -12,7 +12,11 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == *s2)
 	{
 		if (*s1 == '\0' || *s2 == '\0')
-		{	if (*s1 == '\0' && *s2 == '\0')
+		{
+			if (*s2 == '*')
+				return (wildcmp(s1, ++s2));
+
+			if (*s1 == '\0' && *s2 == '\0')
 				return (1);
 			else
 				return (0);
@@ -20,7 +24,11 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(++s1, ++s2));
 	}
 	else if (*s2 == '*')
-	{	
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, ++s2));
+		if (*(s1 + 2) != *(s2 + 3))
+			return (wildcmp(++s1, s2));
 		if (*s1 == *(s2 + 1))
 			return (wildcmp(++s1, s2 + 2));
 		return (wildcmp(++s1, s2));
