4456b03eb75f9bf59789228084f3f7d0e6ce141a
bisrax <bisratx@gmail.com>
2021-08-01 17:50:26 +0300

    yup

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - prints the elements of node.
+ * @h: a linked list head
+ *
+ * Return: the number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	
+	size_t nodesiX;
+
+	for (nodesiX = 0; h != NULL; nodesiX++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("%u %s\n", h->len, h->str);
+	h = h->next;
+	}
+	return (nodesiX);
+}
