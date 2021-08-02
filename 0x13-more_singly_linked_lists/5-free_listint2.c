67b905736962b943fd74f661c54d7a5739326301
bisrax <bisratx@gmail.com>
2021-08-02 12:43:57 +0300

    used two vars

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees the memory of lists.
+ * @head: head of a list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *fr;
+
+	fr = (*head)->next;
+	while (fr)
+	{
+		temp = fr;
+		fr = fr->next;
+		free(temp);
+	}
+	*head = NULL;
+}	
