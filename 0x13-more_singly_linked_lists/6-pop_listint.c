63fe0ae86d6ec2c3fe3c38da8fb8afe4c6cce045
bisrax <bisratx@gmail.com>
2021-08-04 14:35:27 +0300

    handling head NULL

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,12 +8,16 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *temp = *head;
-	int num = (*head)->n;
+	listint_t *temp;
+	int num;
 
-	if (head == NULL)
-		return (0);
-	*head = (*head)->next;
-	free(temp);
-	return (num);
+	if (*head != NULL)
+	{
+		temp = *head;
+		num = (*head)->n;
+		*head = (*head)->next;
+		free(temp);
+		return (num);
+	}
+	else return (0);
 }
