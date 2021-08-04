501aa126115089fb7bcc1e8182bdd05d789ab2a3
bisrax <bisratx@gmail.com>
2021-08-04 12:47:50 +0300

    malloc handling

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,12 +12,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	if (head == NULL)
+	if (head == NULL || new == NULL)
 	{
 		return (NULL);
 	}
+	new->n = n;
+	new->next = *head;
+	*head = new;
 	return (*head);
 }
