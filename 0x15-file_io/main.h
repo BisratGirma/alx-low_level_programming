422b8f84cd2fb02f08b81a6559e3d81826ff386d
bisrax <bisratx@gmail.com>
2021-08-11 12:16:45 +0300

    forgetn main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,8 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-
+int create_file(const char *filename, char *text_content);
 #endif
