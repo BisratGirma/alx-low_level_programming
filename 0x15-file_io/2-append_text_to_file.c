4dcfe567c93413315788511f1888d306d7263466
bisrax <bisratx@gmail.com>
2021-08-11 13:02:39 +0300

    third wave

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: the name of the filename.
+ * @text_content: a string to be added on end of file.
+ *
+ * Return: 1 0n success and -1 on failure.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t r;
+	int i = 0, fd = open(filename, O_WRONLY || O_APPEND);
+
+	if (fd == -1)
+	       return (-1);
+
+	while (*text_content)
+	{
+		i++;
+		text_content++;
+	}
+
+	r = write(fd, text_content, i);
+	close(fd);
+	if (r == -1)
+		return (-1);
+	else
+		return (1);
+}
