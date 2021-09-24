c9baaf90a6899e84823711dc9d764776e0d35257
bisrax <bisratx@gmail.com>
2021-09-25 00:36:39 +0300

    4th

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_dlistint -  frees a list
+ * @head: pointer to head of the list
+ *
+ * Return: Void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+
+	if (head == NULL)
+		;
+	else if (head->next == NULL)
+		free(head);
+	else
+	{
+		while (nex != NULL)
+		{
+			temp = temp->next;
+			free(head);
+			head = temp;
+		}
+	}
+}
